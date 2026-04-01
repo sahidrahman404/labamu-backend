e8b0e82826d3c180d3b537a32cd5bf8ccf71bb95
sahid rahman <sahidrahman404@gmail.com>
2026-04-01 21:53:16 +0700

    init

---
+++ src/Config.ts
@@ -0,0 +1,5 @@
+import { Config } from "effect"
+
+export const AppConfig = Config.all({
+  port: Config.integer("PORT").pipe(Config.withDefault(3000)),
+})
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { BunRuntime } from "@effect/platform-bun"
+import { Effect } from "effect"
+import { App } from "@/App.ts"
+
+const program = Effect.logInfo("Starting server...").pipe(Effect.provide(App))
+
+BunRuntime.runMain(program)
